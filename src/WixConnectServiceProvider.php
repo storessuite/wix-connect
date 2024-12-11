dd626aae452c72739224d68e83bcc418460c3873
sarojkalamagar <38393641+sarojkalamagar@users.noreply.github.com>
2024-12-11 18:53:15 +0545

    Service provider added.

---
+++ src/WixConnectServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace StoresSuite\WixConnect;
+
+use Illuminate\Support\ServiceProvider;
+
+class WixConnectServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services
+     */
+    public function register() {}
+
+    /**
+     * Bootstrap package services
+     */
+    public function boot() {}
+}
